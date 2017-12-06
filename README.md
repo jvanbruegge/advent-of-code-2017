a1490a617f8727917220477346b8badc2ae34524
Jan van Brügge <supermanitu@gmail.com>
2017-12-06 01:57:04 +0100

    Day 5: Haskell solution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # advent-of-code-2017
 My solutions for the advent of code
+
+## Special attention
+
+Day 5 needs to be compiled to not throw an stack overflow exception
---
+++ day5/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import qualified Data.Map.Strict as M
+
+main = do
+    input <- fmap (filter (not . null)) $ lines <$> readFile "day5/input.txt"
+
+    let map = insertLines input
+
+    let result1 = doStep (+1) 0 0 map
+    putStrLn $ "Part 1: Result is " ++ show result1
+
+    let result2 = doStep increment2 0 0 map
+    putStrLn $ "Part 2: Result is " ++ show result2
+
+doStep :: (Int -> Int) -> Int -> Int -> M.Map Int Int -> Int
+doStep incr n pc map = case M.lookup pc map of
+        Nothing -> n
+        Just x -> doStep incr (n + 1) (pc + x) (M.insert pc (incr x) map)
+
+
+increment2 :: Int -> Int
+increment2 prev = if prev >= 3 then prev - 1 else prev + 1
+
+insertLines :: [String] -> M.Map Int Int
+insertLines lines = helper 0 lines M.empty
+    where helper n [] map = map
+          helper n (x:xs) map = helper (n + 1) xs $ M.insert n (read x) map
